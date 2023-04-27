f798f2c70134c81698e31fa9ed562de3d2381f8e
OleksiiPanaioti <cavedzgn@gmail.com>
2023-04-27 13:11:13 +0300

    chages ti Searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,30 +1,52 @@
 import { Component } from 'react';
+import * as yup from 'yup';
+import { Formik } from 'formik';
+import { ImSearch } from 'react-icons/im';
+import { Form, ButtonSubmit, FieldStyled, Span } from './Searchbar.styled';
+
+const initialValues = { name: '' };
+
+const formSchema = yup.object().shape({
+  name: yup.string().trim().required('Required field!'),
+});
 
 export default class Searchbar extends Component {
   state = {
-    pictureId: '',
+    pictureName: '',
+  };
+
+  handleNameChange = e => {
+    this.setState({ pictureName: e.currentTarget.value.toLowerCase() });
   };
 
   handleSubmit = event => {
     event.preventDefault();
+
+    this.setState({ pictureName: '' });
   };
 
   render() {
     return (
       <header class="searchbar">
-        <form class="form" onSubmit={this.handleSubmit}>
-          <button type="submit" class="button">
-            <span class="button-label">Search</span>
-          </button>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={formSchema}
+          onSubmit={this.handleSubmit}
+        >
+          <Form htmlFor="name">
+            <ButtonSubmit type="submit">
+              <Span>Search</Span>
+            </ButtonSubmit>
 
-          <input
-            class="input"
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-          />
-        </form>
+            <FieldStyled
+              type="text"
+              autocomplete="off"
+              autofocus
+              placeholder="Search images and photos"
+              onChange={this.handleNameChange}
+            />
+          </Form>
+        </Formik>
       </header>
     );
   }
