2f4498c9df0da47f6372ae6143aea27970d6f654
OleksiiPanaioti <cavedzgn@gmail.com>
2023-05-08 11:19:02 +0300

    error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
+
 import { ToastContainer, toast } from 'react-toastify';
 
 import SearchingBar from './Searchbar/Searchbar';
@@ -13,125 +13,6 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import fetchApi from './ServiceApi/ServiceApi';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-// export default class App extends Component {
-//   static propTypes = { searchQuery: PropTypes.string };
-
-//   state = {
-//     searchQuery: '',
-//     images: [],
-//     page: 1,
-//     selectedImage: null,
-//     alt: null,
-//     status: 'idle',
-//     error: null,
-//   };
-
-//   totalHits = null;
-
-//   async componentDidUpdate(_, prevState) {
-//     const { page, searchQuery } = this.state;
-
-//     if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
-//       this.setState({ status: 'pending' });
-
-//       try {
-//         const imageData = await fetchApi(searchQuery, page);
-
-//         this.totalHits = imageData.total;
-
-//         const imagesHits = imageData.hits;
-
-//         if (!imagesHits.length) {
-//           toast.warning('havent found anything, try smthg else.');
-//         }
-
-//         this.setState(({ images }) => ({
-//           images: [...images, ...imagesHits],
-//           status: 'resolved',
-//         }));
-//       } catch (error) {
-//         toast.error(`Smthg went wrong, we re so sorry. ${error.message}`);
-//         this.setState({ status: 'rejected' });
-//       }
-//     }
-//   }
-
-//   handleFormSubmit = searchQuery => {
-//     if (this.state.searchQuery === searchQuery) {
-//       return;
-//     }
-//     this.resetState();
-//     this.setState({ searchQuery });
-//   };
-
-//   handleSelectedImage = (largeImageUrl, tags) => {
-//     this.setState({
-//       selectedImage: largeImageUrl,
-//       alt: tags,
-//     });
-//   };
-
-//   resetState = () => {
-//     this.setState({
-//       searchQuery: '',
-//       page: 1,
-//       images: [],
-//       selectedImage: null,
-//       alt: null,
-//       status: 'idle',
-//     });
-//   };
-
-//   loadMore = () => {
-//     this.setState(prevState => ({
-//       page: prevState.page + 1,
-//     }));
-//   };
-
-//   closeModal = () => {
-//     this.setState({
-//       selectedImage: null,
-//     });
-//   };
-
-//   // ---->
-
-//   render() {
-//     const { images, status, selectedImage, alt, error } = this.state;
-//     return (
-//       <Container>
-//         <SearchingBar onSubmit={this.handleFormSubmit} />
-//         <ToastContainer autoClose={3000} theme="colored" pauseOnHover />
-//         {status === 'pending' && <Spiner />}
-//         {error && (
-//           <h1 style={{ color: 'orangered', textAlign: 'center' }}>
-//             {error.message}
-//           </h1>
-//         )}
-
-//         {images.length > 0 && (
-//           <ImageGallery
-//             images={images}
-//             selectedImage={this.handleSelectedImage}
-//           />
-//         )}
-
-//         {images.length > 0 && images.length !== this.totalHits && (
-//           <LoadMoreButton onClick={this.loadMore} />
-//         )}
-
-//         {selectedImage && (
-//           <Modal
-//             selectedImage={selectedImage}
-//             tags={alt}
-//             onClose={this.closeModal}
-//           />
-//         )}
-//       </Container>
-//     );
-//   }
-// }
 
 export default function App() {
   const [searchQuery, setSearchQuery] = useState('');
