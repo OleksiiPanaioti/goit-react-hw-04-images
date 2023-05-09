6ea518df0ae16afae374a64d9b6d936765013b9d
OleksiiPanaioti <cavedzgn@gmail.com>
2023-05-09 09:59:32 +0300

    changed to function

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -7,42 +7,75 @@ import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#root');
 
-export default class Modal extends Component {
-  static propTypes = {
-    selectedImage: PropTypes.string,
-    tags: PropTypes.string,
-    onClose: PropTypes.func,
-  };
+// export default class Modal extends Component {
+//   static propTypes = {
+//     selectedImage: PropTypes.string,
+//     tags: PropTypes.string,
+//     onClose: PropTypes.func,
+//   };
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
 
-  handleKeyDown = e => {
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     const { selectedImage, tags } = this.props;
+
+//     return createPortal(
+//       <ModalOverlay onClick={this.handleBackdropClick}>
+//         <ModalContainer>
+//           <img src={selectedImage} alt={tags} />
+//         </ModalContainer>
+//       </ModalOverlay>,
+//       modalRoot
+//     );
+//   }
+// }
+
+export default function Modal({ selectedImage, tags, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  });
+
+  const handleKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    const { selectedImage, tags } = this.props;
-
-    return createPortal(
-      <ModalOverlay onClick={this.handleBackdropClick}>
-        <ModalContainer>
-          <img src={selectedImage} alt={tags} />
-        </ModalContainer>
-      </ModalOverlay>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <ModalOverlay onClick={handleBackdropClick}>
+      <ModalContainer>
+        <img src={selectedImage} alt={tags} />
+      </ModalContainer>
+    </ModalOverlay>,
+    modalRoot
+  );
 }
+Modal.propTypes = {
+  selectedImg: PropTypes.string,
+  tags: PropTypes.string,
+  onClose: PropTypes.func,
+};
